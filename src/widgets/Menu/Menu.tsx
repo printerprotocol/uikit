3d0b7db7deb95b54077417e39af87f101466a8ba
PrinterDev <80286557+printerprotocol@users.noreply.github.com>
2021-04-12 10:36:03 -0300

    husky test

--- src/widgets/Menu/Menu.tsx
+++ src/widgets/Menu/Menu.tsx
@@ -120,7 +120,7 @@ const Menu: React.FC<NavProps> = ({
           isPushed={isPushed}
           togglePush={() => setIsPushed((prevState: boolean) => !prevState)}
           isDark={isDark}
-          href={homeLink?.href ?? "/?"}
+          href={homeLink?.href ?? "/"}
         />
         <Flex>
           <UserBlock account={account} login={login} logout={logout} />
